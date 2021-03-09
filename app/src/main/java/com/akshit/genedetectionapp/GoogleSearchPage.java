1780018075269739d5e0ca6a3c203986ae694e3c
Amisha Sahu <amishasahu328@gmail.com>
2021-03-09 15:47:33 +0530

    Added Black StatusBar Successfully

--- app/src/main/java/com/akshit/genedetectionapp/GoogleSearchPage.java
+++ app/src/main/java/com/akshit/genedetectionapp/GoogleSearchPage.java
@@ -8,6 +8,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class GoogleSearchPage extends AppCompatActivity {
     WebView wb;
@@ -18,24 +19,18 @@ public class GoogleSearchPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_search_page);
         wb=findViewById(R.id.wb1);
-        b1=findViewById(R.id.b1);
-        b2=findViewById(R.id.b2);
+        TextView close=findViewById(R.id.close);
         WebSettings ws=wb.getSettings();
         wb.getSettings().setBuiltInZoomControls(true);
         ws.setJavaScriptEnabled(true);
         wb.setWebViewClient(new WebViewClient());
         wb.loadUrl("https://www.google.com");
-        b1.setOnClickListener(new View.OnClickListener() {
+        close.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                wb.goBack();
+            public void onClick(View view) {
+                finish();
             }
         });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                wb.goForward();
-            }
-        });
+
     }
 }
--- app/src/main/java/com/akshit/genedetectionapp/NucleotideBlast.java
+++ app/src/main/java/com/akshit/genedetectionapp/NucleotideBlast.java
@@ -8,6 +8,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class NucleotideBlast extends AppCompatActivity {
     WebView wb;
@@ -18,23 +19,16 @@ public class NucleotideBlast extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nucleotide_blast);
         wb=findViewById(R.id.wb1);
-        b1=findViewById(R.id.b1);
-        b2=findViewById(R.id.b2);
+        TextView close=findViewById(R.id.close);
         WebSettings ws=wb.getSettings();
         wb.getSettings().setBuiltInZoomControls(true);
         ws.setJavaScriptEnabled(true);
         wb.setWebViewClient(new WebViewClient());
         wb.loadUrl("https://blast.ncbi.nlm.nih.gov/Blast.cgi");
-        b1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    wb.goBack();
-                }
-            });
-        b2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                wb.goForward();
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
