ef09c94957c1a28ac7fc1209be3eafd2c57bdef3
Akshit Mangotra <akshitmangotra@gmail.com>
2021-04-01 07:56:03 +0530

    Final Commit

--- app/src/main/java/com/akshit/genedetectionapp/fragment_about.java
+++ app/src/main/java/com/akshit/genedetectionapp/fragment_about.java
@@ -19,6 +19,9 @@ public class fragment_about extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+
+
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
